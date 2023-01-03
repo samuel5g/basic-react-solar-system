c95432fb6e79d5fc7c7d9de6fef317944904107d
samuel5g <samuelmonteiro21@gmail.com>
2023-01-03 17:46:16 -0300

    feat(component): :sparkles: Adds Missions component and import in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header/>
       <SolarSystem/>
+      <Missions/>
     </>
   );
 }
---
+++ src/components/Missions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Title from './Title'
+
+function Missions() {
+  return (
+    <div><Title headline='Missões'/></div>
+  )
+}
+
+export default Missions
