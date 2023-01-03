f6db26f77aa11c37769fec8b43fa71336e1cca38
samuel5g <samuelmonteiro21@gmail.com>
2023-01-03 15:54:22 -0300

    feat(component): :sparkles: Adds Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+function Title({headline}) {
+  return (
+    <h2>{headline}</h2>
+  )
+}
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired
+}
+export default Title
