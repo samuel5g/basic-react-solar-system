db791bef91c0313221da3ee9bd1773a9be1493e2
samuel5g <samuelmonteiro21@gmail.com>
2023-01-03 17:43:15 -0300

    feat(component): :sparkles: Adds planets with name and image in solar system

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Title from './Title'
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 function SolarSystem() {
   return (
     <div>
       <Title headline='Planetas'/>
+      {planets.map(({name, image,}) => <PlanetCard planetImage={image} planetName={name}/>)}
     </div>
   )
 }
